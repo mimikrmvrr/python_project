53da6e1c172bc77219c1857df85911a79616198f
Mariya Miteva <mimi_9204@abv.bg>
2013-06-21 12:35:49 +0300

    initial commit

---
+++ python_project/urls.py
@@ -0,0 +1,17 @@
+﻿from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'python_project.views.home', name='home'),
+    # url(r'^python_project/', include('python_project.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
