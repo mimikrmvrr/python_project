4c2d5faa20a4ca9122c7d2f15b2f0a5edd6a72e6
Mariya Miteva <mimi_9204@abv.bg>
2013-08-30 23:30:41 +0300

    Update tests.py

--- my_calendar/tests.py
+++ my_calendar/tests.py
@@ -4,9 +4,11 @@ when you run "manage.py test".
 
 Replace this with more appropriate tests for your application.
 """
+import datetime
 
 from django.test import TestCase
-
+from my_calendar.views import earlier_date
+from django.test.client import Client
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -14,3 +16,17 @@ class SimpleTest(TestCase):
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
+class TimeTests(TestCase):
+    def test_earlier_date(self):
+        """ Tests that date1 is earler."""
+        date1 = datetime(2013, 5, 20, 0, 0)
+        date2 = datetime(2013, 6, 19, 0, 0)
+        self.assertEqual(date1, earlier_date(date1, date2))
+        self.assertEqual(date1, earlier_date(date2, date1))
+
+class ClientTest(TestCase):
+    def test_response(self):
+        c = Client()
+        response = c.post('login/')
+        self.assertEqual(response.status_code, 200)
