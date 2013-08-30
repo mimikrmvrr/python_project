4205c9364336e7cfc518fb2efa4b68e575386089
Mariya Miteva <mimi_9204@abv.bg>
2013-08-30 23:33:26 +0300

    pep8

--- my_calendar/tests.py
+++ my_calendar/tests.py
@@ -10,12 +10,14 @@ from django.test import TestCase
 from my_calendar.views import earlier_date
 from django.test.client import Client
 
+
 class SimpleTest(TestCase):
     def test_basic_addition(self):
         """
         Tests that 1 + 1 always equals 2.
         """
         self.assertEqual(1 + 1, 2)
+
 
 class TimeTests(TestCase):
     def test_earlier_date(self):
@@ -24,6 +26,7 @@ class TimeTests(TestCase):
         date2 = datetime(2013, 6, 19, 0, 0)
         self.assertEqual(date1, earlier_date(date1, date2))
         self.assertEqual(date1, earlier_date(date2, date1))
+
 
 class ClientTest(TestCase):
     def test_response(self):
